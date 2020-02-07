7a04acbee4116266a3f046111a17f9085915b661
alina nikolaenkova <lina.y2018@ya.ru>
2020-02-07 18:53:11 +0300

    Issue #13 SQL scripts to create Order and Invoice

--- src/sql/Create_DB.sql
+++ src/sql/Create_DB.sql
@@ -1,32 +1,107 @@
 BEGIN;
 
-CREATE TABLE User (
-    Id serial PRIMARY KEY,
-    Email text NOT NULL,
+CREATE TABLE User
+(
+    Id            serial PRIMARY KEY,
+    Email         text NOT NULL,
     Password_hash text NOT NULL,
-    Birth_date date NOT NULL,
-    Signup_date date NOT NULL
+    Birth_date    date NOT NULL,
+    Signup_date   date NOT NULL
 );
 
-CREATE TABLE Client (
-    Id int PRIMARY KEY
-        REFERENCES User(Id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-    Pass_id int
-        REFERENCES Passport(Id)
-        ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
+
+
+CREATE TABLE Client
+(
+    Id            int PRIMARY KEY
+        REFERENCES User (Id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+    Pass_id       int
+        REFERENCES Passport (Id)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
     Contact_phone text,
-    Address text
+    Address       text
 );
 
-CREATE TABLE Admin (
+CREATE TABLE Admin
+(
     Id int PRIMARY KEY
-        REFERENCES User(Id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+        REFERENCES User (Id)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
 );
+----------------------------------------
+CREATE TABLE Car
+(
+    Id int PRIMARY KEY
+);
+
+CREATE TABLE Car_Model
+(
+    Id int PRIMARY KEY
+);
+----------------------------------------
+CREATE TYPE e_status_order AS ENUM (
+    'open',
+    'canceled',
+    'approved',
+    'waiting for payment',
+    'close');
+
+CREATE TYPE e_status_invoice AS ENUM (
+    'open',
+    'close');
+
+CREATE TABLE Ord
+(
+    Id              serial PRIMARY KEY,
+    Car_id          int
+        REFERENCES Car (Id)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    Client_id       int
+        REFERENCES Client (Id)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    Admin_id        int
+        REFERENCES Admin (Id)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    Status          e_status_order NOT NULL,
+    Rent_start_date date           NOT NULL,
+    Rent_end_date   date           NOT NULL
+);
+
+CREATE TABLE Invoice
+(
+    Id           serial PRIMARY KEY,
+    Order_id     int
+        REFERENCES Ord (Id)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    Issuer_id    int
+        REFERENCES Client (Id)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    Issue_date   date             NOT NULL,
+    Payment_date date             NOT NULL,
+    Total_price  int              NOT NULL,
+    Status       e_status_invoice NOT NULL
+);
+----------------------------------------
+CREATE TABLE Passport
+(
+    Id int PRIMARY KEY
+);
+
+CREATE TABLE Car_damage
+(
+    Id int PRIMARY KEY
+);
+
+
 
 /* TODO: other tables here */
 
