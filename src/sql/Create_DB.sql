c158483ca9f09cf9bdfd715ef186ec32abdfd62f
Andrej Deriabin <aderiabin.98@gmail.com>
2020-02-10 15:37:06 +0300

    Issue #13 Updated table fields to match ER scheme. Unified code style. Fixed SQL errors.

--- src/sql/Create_DB.sql
+++ src/sql/Create_DB.sql
@@ -5,13 +5,22 @@ CREATE TYPE e_role_users AS ENUM (
     'admin'
 );
 
+CREATE TABLE Passport (
+    Id int PRIMARY KEY,
+    Issue_country text NOT NULL,
+    Issuer text,
+    Issue_date date NOT NULL,
+    Expiration_date date NOT NULL,
+    Serial_number text NOT NULL
+);
+
 CREATE TABLE Users (
     Id serial PRIMARY KEY,
     Email text NOT NULL,
     Password_hash text NOT NULL,
     Role e_role_users NOT NULL,
     Name text NOT NULL,
-    Birth_date  date NOT NULL,
+    Birth_date date NOT NULL,
     Signup_date date NOT NULL,
     Pass_id int
         REFERENCES Passport(Id)
@@ -19,112 +28,90 @@ CREATE TABLE Users (
         ON UPDATE NO ACTION,
     Contact_phone text,
     Address text,
-    Is_blocked boolean NOT NULL
+    Is_blocked boolean NOT NULL,
+    Reputation numeric NOT NULL
 );
 
-----------------------------------------
 CREATE TABLE Car_model (
     Id int PRIMARY KEY,
     Brand_name text NOT NULL,
     Model_name text NOT NULL,
-    Price_per_hour double precision NOT NULL
+    Price_per_hour numeric NOT NULL
 );
 
-CREATE TYPE car_status as ENUM ('broken','taken','free');
+CREATE TYPE car_status as ENUM ( 'broken', 'taken', 'free' );
 
 CREATE TABLE Car (
     Id int PRIMARY KEY,
-    Model_id int
+    Model_id int NOT NULL
         REFERENCES Car_model(Id)
         ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
+        ON UPDATE CASCADE,
     Color text NOT NULL,
     REG_number text NOT NULL,
     VIN_number text NOT NULL,
     Status car_status NOT NULL
 );
-----------------------------------------
+
 CREATE TYPE e_status_order AS ENUM (
     'open',
     'canceled',
     'approved',
     'waiting for payment',
-    'close');
+    'close'
+);
 
-CREATE TYPE e_status_invoice AS ENUM (
-    'open',
-    'close');
-
-CREATE TABLE Ord
-(
-    Id              serial PRIMARY KEY,
-    Car_id          int
+CREATE TABLE Orders (
+    Id serial PRIMARY KEY,
+    Car_id int NOT NULL
         REFERENCES Car (Id)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    Client_id       int
-        REFERENCES Client (Id)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    Admin_id        int
-        REFERENCES Admin (Id)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    Status          e_status_order NOT NULL,
-    Rent_start_date date           NOT NULL,
-    Rent_end_date   date           NOT NULL
+        ON DELETE NO ACTION
+        ON UPDATE CASCADE,
+    Client_id int NOT NULL
+        REFERENCES Users (Id)
+        ON DELETE NO ACTION
+        ON UPDATE CASCADE,
+    Admin_id int
+        REFERENCES Users (Id)
+        ON DELETE NO ACTION
+        ON UPDATE CASCADE,
+    Status e_status_order NOT NULL,
+    Rent_start_date date NOT NULL,
+    Rent_end_date date NOT NULL,
+    Discount numeric NOT NULL
 );
 
-CREATE TABLE Invoice
-(
-    Id           serial PRIMARY KEY,
-    Order_id     int
-        REFERENCES Ord (Id)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    Issuer_id    int
-        REFERENCES Client (Id)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    Issue_date   date             NOT NULL,
-    Payment_date date             NOT NULL,
-    Total_price  int              NOT NULL,
-    Status       e_status_invoice NOT NULL
+CREATE TYPE e_status_invoice AS ENUM ( 'open', 'close' );
+
+CREATE TABLE Invoice (
+    Id serial PRIMARY KEY,
+    Order_id int NOT NULL
+        REFERENCES Orders (Id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    Issue_date date NOT NULL,
+    Payment_date date NOT NULL,
+    Total_price int NOT NULL,
+    Status e_status_invoice NOT NULL
 );
-----------------------------------------
+
 CREATE TYPE damage_status AS ENUM (
     'fixed',
     'not fixed',
-    'in process');
-CREATE TABLE Passport
-(
-    Id int PRIMARY KEY,
-    Issue_country text NOT NULL ,
-    Issuer text ,
-    Issue_date date NOT NULL ,
-    Expiration_date date NOT NULL ,
-    Serial_number int NOT NULL
+    'under repair'
 );
 
-CREATE TABLE Car_damage
-(
+CREATE TABLE Car_damage (
     Id int PRIMARY KEY,
-    Order_id int
-        REFERENCES Ord (Id)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    Type text,
+    Order_id int NOT NULL
+        REFERENCES Orders (Id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
     Description text NOT NULL,
     Date date NOT NULL,
-    Severity int NOT NULL ,
+    Severity int NOT NULL,
     Repair_cost int NOT NULL,
-    Status text NOT NULL
+    Status damage_status NOT NULL
 );
-
-
-
-/* TODO: other tables here */
-
-/* TODO: data creation procedures here */
 
 COMMIT;
