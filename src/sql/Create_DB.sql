3025371b30f597dfd450868f7cb471cd4f5480a3
Andrej Deriabin <aderiabin.98@gmail.com>
2020-02-12 14:51:51 +0300

    Issue #13 Fixed all issues after reverting/merging.

--- src/sql/Create_DB.sql
+++ src/sql/Create_DB.sql
@@ -14,6 +14,13 @@ CREATE TABLE Passport (
     Serial_number text NOT NULL
 );
 
+CREATE TABLE Driving_license (
+    Id int PRIMARY KEY,
+    Issue_date date NOT NULL,
+    Expiration_date date,
+    Serial_number text NOT NULL
+);
+
 CREATE TABLE Users (
     Id serial PRIMARY KEY,
     Email text NOT NULL,
@@ -25,7 +32,7 @@ CREATE TABLE Users (
     Pass_id int
         REFERENCES Passport(Id)
         ON DELETE NO ACTION
-        ON UPDATE NO ACTION,
+        ON UPDATE CASCADE,
     Driving_license_id int
         REFERENCES Driving_license(Id)
         ON DELETE NO ACTION
@@ -116,12 +123,6 @@ CREATE TABLE Car_damage (
     Severity int NOT NULL,
     Repair_cost int NOT NULL,
     Status damage_status NOT NULL
-);
-CREATE TABLE Driving_license (
-    Id int PRIMARY KEY,
-    Issue_date date NOT NULL,
-    Expiration_date date,
-    Serial_number text NOT NULL
 );
 
 COMMIT;
