7be7c30a9cf1535ae4db61969b44a1ffcaecb6a8
nikaalin <lina.y2018@ya.ru>
2020-02-17 10:41:50 +0300

    fixed InvoiceEntity   - changed totalPrice type   - added field status   - added braces

--- src/main/java/com/ifmo/epampractice/entity/InvoiceEntity.java
+++ src/main/java/com/ifmo/epampractice/entity/InvoiceEntity.java
@@ -1,5 +1,8 @@
 package com.ifmo.epampractice.entity;
 
+import com.ifmo.epampractice.enums.InvoiceStatus;
+
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -8,34 +11,41 @@ public class InvoiceEntity {
     private int orderId;
     private Date issueDate;
     private Date paymentDate;
-    private int totalPrice;
+    private BigDecimal totalPrice;
+    private InvoiceStatus status;
 
     public InvoiceEntity() {
     }
 
-    public InvoiceEntity(int id, int orderId, Date issueDate, Date paymentDate, int totalPrice) {
+    public InvoiceEntity(int id, int orderId, Date issueDate, Date paymentDate, BigDecimal totalPrice, InvoiceStatus status) {
         this.id = id;
         this.orderId = orderId;
         this.issueDate = issueDate;
         this.paymentDate = paymentDate;
         this.totalPrice = totalPrice;
+        this.status = status;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof InvoiceEntity)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof InvoiceEntity)) {
+            return false;
+        }
         InvoiceEntity that = (InvoiceEntity) o;
         return id == that.id &&
                 orderId == that.orderId &&
-                totalPrice == that.totalPrice &&
+                Objects.equals(totalPrice, that.totalPrice) &&
                 Objects.equals(issueDate, that.issueDate) &&
-                Objects.equals(paymentDate, that.paymentDate);
+                Objects.equals(paymentDate, that.paymentDate) &&
+                Objects.equals(status, that.status);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, orderId, issueDate, paymentDate, totalPrice);
+        return Objects.hash(id, orderId, issueDate, paymentDate, totalPrice, status);
     }
 
     public int getId() {
@@ -70,11 +80,19 @@ public class InvoiceEntity {
         this.paymentDate = paymentDate;
     }
 
-    public int getTotalPrice() {
+    public BigDecimal getTotalPrice() {
         return totalPrice;
     }
 
-    public void setTotalPrice(int totalPrice) {
+    public void setTotalPrice(BigDecimal totalPrice) {
         this.totalPrice = totalPrice;
+    }
+
+    public InvoiceStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(InvoiceStatus status) {
+        this.status = status;
     }
 }
