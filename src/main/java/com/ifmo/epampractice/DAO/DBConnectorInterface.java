647d8c4d3e314f9f376d066f9fb970c12fdba6fd
nikaalin <lina.y2018@ya.ru>
2020-02-17 17:04:07 +0300

    fixed
    - DBConnectorPostgres's Singleton
    - added method addConnection()

--- src/main/java/com/ifmo/epampractice/DAO/DBConnectorInterface.java
+++
@@ -1,9 +0,0 @@
-package com.ifmo.epampractice.DAO;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-
-public interface DBConnectorInterface {
-    void setProperties(String url) throws IOException;
-    DataSource getDataSource();
-}
---
+++ src/main/java/com/ifmo/epampractice/dao/DBConnectorPostgres.java
@@ -0,0 +1,50 @@
+package com.ifmo.epampractice.dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+public class DBConnectorPostgres implements DBConnectorInterface {
+    private static DBConnectorPostgres instance;
+
+    private final String propertiesUrl = "/properties/dbPostgres.properties";
+    private Properties properties = new Properties();
+    private PGSimpleDataSource dataSource = new PGSimpleDataSource();
+
+    private DBConnectorPostgres() {
+        try {
+            dataSource = new PGSimpleDataSource();
+            setProperties();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setProperties() throws IOException {
+        properties.load(this.getClass().getResourceAsStream(propertiesUrl));
+        dataSource.setUrl(properties.getProperty("URL"));
+        dataSource.setUser(properties.getProperty("USERNAME"));
+        dataSource.setPassword(properties.getProperty("PASSWORD"));
+
+    }
+
+    public static synchronized DBConnectorPostgres getInstance(){
+        if(instance == null){
+            instance = new DBConnectorPostgres();
+        }
+        return instance;
+    }
+
+    @Override
+    public Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
