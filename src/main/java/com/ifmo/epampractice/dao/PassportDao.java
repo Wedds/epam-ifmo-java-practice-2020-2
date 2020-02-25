d0a2ccbc90b2104530d5010840b2d63e5fed7bcc
ChirkinaTanya <tany@chirkin.ru>
2020-02-25 15:56:22 +0300

    Issue #75 Created PassportDao and PassportDao Interface

---
+++ src/main/java/com/ifmo/epampractice/dao/PassportDao.java
@@ -0,0 +1,21 @@
+package com.ifmo.epampractice.dao;
+import com.ifmo.epampractice.entity.PassportEntity;
+
+import java.util.List;
+
+public interface PassportDao extends BasicDaoInterface<PassportEntity> {
+    @Override
+    List<PassportEntity> getAll();
+
+    @Override
+    PassportEntity get(int id);
+
+    @Override
+    void update(PassportEntity PassportEntity);
+
+    @Override
+    void delete(PassportEntity PassportEntity);
+
+    @Override
+    void save(PassportEntity PassportEntity);
+}
