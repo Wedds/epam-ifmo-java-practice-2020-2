0b19e50046dde17ffd5de5d91f929cdef1ddeb3d
Andrej Deriabin <aderiabin.98@gmail.com>
2020-02-25 21:54:11 +0300

    Issue #68 Implemented SignUpService (no existing user check yet because it's not implemented in DAO).

---
+++ src/main/java/com/ifmo/epampractice/exceptions/SignUpException.java
@@ -0,0 +1,7 @@
+package com.ifmo.epampractice.exceptions;
+
+public class SignUpException extends Exception {
+    public SignUpException(String errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ src/main/java/com/ifmo/epampractice/services/SignUpService.java
@@ -0,0 +1,15 @@
+package com.ifmo.epampractice.services;
+
+import com.ifmo.epampractice.entity.UsersEntity;
+import com.ifmo.epampractice.enums.UserRole;
+import com.ifmo.epampractice.exceptions.SignUpException;
+import com.ifmo.epampractice.security.SecureString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public interface SignUpService {
+    UsersEntity signUp(String email, SecureString password, String name,
+                       LocalDate birthDate, String phone, String address,
+                       UserRole role, BigDecimal reputation) throws SignUpException;
+}
