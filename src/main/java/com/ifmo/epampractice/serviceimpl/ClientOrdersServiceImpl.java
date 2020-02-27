f45f1eb0b38fe717e70ee45b6a682c1abd9dbb84
nikaalin <lina.y2018@ya.ru>
2020-02-27 18:28:10 +0300

    Issue #80 Added calcCost(orderId) and getInvoiceByOrderId(orderId) methods

--- src/main/java/com/ifmo/epampractice/serviceimpl/ClientOrdersServiceImpl.java
+++ src/main/java/com/ifmo/epampractice/serviceimpl/ClientOrdersServiceImpl.java
@@ -1,17 +1,21 @@
 package com.ifmo.epampractice.serviceimpl;
 
+import com.ifmo.epampractice.dao.CarModelDao;
 import com.ifmo.epampractice.dao.InvoiceDao;
 import com.ifmo.epampractice.dao.OrderDao;
 import com.ifmo.epampractice.dao.UsersDao;
+import com.ifmo.epampractice.daoimpl.CarModelDaoImpl;
 import com.ifmo.epampractice.daoimpl.InvoiceDaoImpl;
 import com.ifmo.epampractice.daoimpl.OrderDaoImpl;
 import com.ifmo.epampractice.daoimpl.UsersDaoImpl;
+import com.ifmo.epampractice.entity.CarModelEntity;
 import com.ifmo.epampractice.entity.InvoiceEntity;
 import com.ifmo.epampractice.entity.OrderEntity;
 import com.ifmo.epampractice.entity.UsersEntity;
 import com.ifmo.epampractice.enums.OrderStatus;
 import com.ifmo.epampractice.services.ClientOrdersService;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -26,6 +30,12 @@ public class ClientOrdersServiceImpl implements ClientOrdersService {
     public List<InvoiceEntity> getAllInvoices(int userId) {
         InvoiceDao dao = new InvoiceDaoImpl();
         return dao.getAllByUserId(userId);
+    }
+
+    @Override
+    public InvoiceEntity getInvoiceByOrderId(int orderId) {
+        InvoiceDao dao = new InvoiceDaoImpl();
+        return dao.getByOrderId(orderId);
     }
 
     @Override
@@ -57,6 +67,15 @@ public class ClientOrdersServiceImpl implements ClientOrdersService {
 
     }
 
+    @Override
+    public BigDecimal calcCost(int orderId) {
+        OrderDao dao = new OrderDaoImpl();
+        OrderEntity order = dao.get(orderId);
+        BigDecimal cost = getPrimaryCost(order).add(getInvoiceCost(order));
+
+        return cost;
+    }
+
     private boolean accessToCancel(OrderEntity order) {
         return (order.getStatus() == OrderStatus.OPEN);
     }
@@ -66,5 +85,22 @@ public class ClientOrdersServiceImpl implements ClientOrdersService {
         UsersEntity user = dao.get(clientId);
 
         return (user.getDrivingLicenseId() != 0 & user.getPassId() != 0);
+    }
+
+    private BigDecimal getPrimaryCost(OrderEntity order) {
+        BigDecimal primaryCost;
+        int hours = order.getRentEndDate().getHours() - order.getRentStartDate().getHours();
+        BigDecimal bdHours = BigDecimal.valueOf(hours);
+
+        CarModelDao dao = new CarModelDaoImpl();
+        CarModelEntity car = dao.get(order.getCarId());
+        primaryCost = bdHours.multiply(car.getPricePerHour());
+        return primaryCost;
+    }
+
+    private BigDecimal getInvoiceCost(OrderEntity order){
+        InvoiceDao dao = new InvoiceDaoImpl();
+        InvoiceEntity invoice = dao.getByOrderId(order.getId());
+        return invoice.getTotalPrice();
     }
 }
--- src/main/java/com/ifmo/epampractice/services/ClientOrdersService.java
+++ src/main/java/com/ifmo/epampractice/services/ClientOrdersService.java
@@ -3,12 +3,17 @@ package com.ifmo.epampractice.services;
 import com.ifmo.epampractice.entity.InvoiceEntity;
 import com.ifmo.epampractice.entity.OrderEntity;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
 public interface ClientOrdersService {
     List<OrderEntity> getAllOrders(int userId);
     List<InvoiceEntity> getAllInvoices(int userId);
+    InvoiceEntity getInvoiceByOrderId(int orderId);
+
     boolean newOrder(int carId, int clientId, Date rentStartDate, Date rentEndDate);
     boolean cancelOrder(int orderId);
+
+    BigDecimal calcCost(int orderId);
 }
