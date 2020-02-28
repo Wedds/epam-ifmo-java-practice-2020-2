a0a0eef86b81b75f1cfcfae995c7c72d8e326ab7
Andrej Deriabin <aderiabin.98@gmail.com>
2020-02-28 11:33:11 +0300

    Issue #88 Finished SignUpController. Created stub signup page. Fixed project structure to be able to deploy it locally.

--- src/main/java/com/ifmo/epampractice/controllers/SignUpController.java
+++ src/main/java/com/ifmo/epampractice/controllers/SignUpController.java
@@ -1,23 +1,43 @@
 package com.ifmo.epampractice.controllers;
 
+import com.ifmo.epampractice.dao.UsersDao;
+import com.ifmo.epampractice.daoimpl.UsersDaoImpl;
+import com.ifmo.epampractice.enums.UserRole;
+import com.ifmo.epampractice.security.SecureString;
+import com.ifmo.epampractice.serviceimpl.PasswordHashServiceImpl;
+import com.ifmo.epampractice.serviceimpl.SignUpServiceImpl;
+import com.ifmo.epampractice.services.PasswordHashService;
+import com.ifmo.epampractice.services.SignUpService;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 @WebServlet(name = "SignUpController", urlPatterns = "/signup")
 public class SignUpController extends HttpServlet {
 
+    private UsersDao usersDao = new UsersDaoImpl();
+    private PasswordHashService passwordHashService = new PasswordHashServiceImpl();
+    private SignUpService signUpService = new SignUpServiceImpl(usersDao, passwordHashService);
+
+    private static final BigDecimal INITIAL_REPUTATION = new BigDecimal(1.0);
+
     private static final DateTimeFormatter dateFormatter
             = DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        //TODO: display some kind of signup page or whatever
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("signup.jsp");
+        dispatcher.forward(req, resp);
     }
 
     @Override
@@ -25,15 +45,24 @@ public class SignUpController extends HttpServlet {
             throws ServletException, IOException {
 
         String email = req.getParameter("email");
-        String password = req.getParameter("password"); //TODO: How do we get is as char[] array?
+        String passwordStr = req.getParameter("password"); //TODO: How do we get is as char[] array?
         String name = req.getParameter("name");
         String birthDateStr = req.getParameter("birthDate");
         String phone = req.getParameter("phone");
         String address = req.getParameter("address");
 
-        LocalDate birthDate = LocalDate.parse(birthDateStr, dateFormatter);
-        //TODO: merge SignUpService into current branch and call it here
+        SecureString password = new SecureString();
+        password.append(passwordStr.toCharArray());
 
-        //TODO: display signup result
+        LocalDate birthDate = LocalDate.parse(birthDateStr, dateFormatter);
+        if (signUpService.userExists(email)) {
+            resp.sendRedirect("/signup?error=1");
+            return;
+        }
+
+        signUpService.signUp(email, password, name, birthDate, phone, address,
+                UserRole.CLIENT, INITIAL_REPUTATION);
+
+        resp.sendRedirect("/signup?success=1");
     }
 }
---
+++ src/main/webapp/signup.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Sign up</title>
+</head>
+<body>
+    TODO: sign up form here
+</body>
+</html>
