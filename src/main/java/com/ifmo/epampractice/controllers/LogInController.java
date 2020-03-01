3bc4084287653a67d60059ff769f62b75966ff8b
nikaalin <lina.y2018@ya.ru>
2020-03-01 20:22:28 +0300

    Issue #96 Created controller and page

---
+++ src/main/java/com/ifmo/epampractice/controllers/LogInController.java
@@ -0,0 +1,52 @@
+package com.ifmo.epampractice.controllers;
+
+import com.ifmo.epampractice.dao.UsersDao;
+import com.ifmo.epampractice.daoimpl.UsersDaoImpl;
+import com.ifmo.epampractice.entity.UsersEntity;
+import com.ifmo.epampractice.security.SecureString;
+import com.ifmo.epampractice.serviceimpl.LogInServiceImpl;
+import com.ifmo.epampractice.services.LogInService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "LogInController", urlPatterns = "/login")
+public class LogInController extends HttpServlet {
+    private LogInService logInService = new LogInServiceImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("login.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+
+        String email = req.getParameter("email");
+        String passwordStr = req.getParameter("password");
+
+        SecureString password = new SecureString();
+        password.append(passwordStr.toCharArray());
+
+        if (!logInService.getAuth(email, password)) {
+            throw new RuntimeException();
+        }
+        HttpSession session = req.getSession();
+        session.setAttribute("name", email);
+
+        UsersDao usersDao = new UsersDaoImpl();
+        UsersEntity usersEntity = usersDao.getByEmail(email);
+        resp.sendRedirect("/login?id=" + usersEntity.getId());
+    }
+}
+
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,67 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Linay
+  Date: 01.03.2020
+  Time: 15:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" %>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Регистрация</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    >
+
+    <style type="text/css">
+        .signup-row {
+            display: inline-flex;
+            flex-direction: row;
+            width: 100%;
+        }
+
+        .signup-row > span {
+            flex: 1;
+        }
+
+        .signup-row > input {
+            flex: 2;
+        }
+    </style>
+</head>
+<body>
+<div class="jumbotron text-center">
+    <h1>Вход</h1>
+    <p>Войдите для оформления заказа</p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+            <form method="POST" action="${pageContext.request.contextPath}/login">
+                <div class="signup-row">
+                    <span>Email:</span>
+                    <input type="text" name="email" required placeholder="hello@world.com"/>
+                </div>
+                <div class="signup-row">
+                    <span>Пароль:</span>
+                    <input type="password" name="password" required placeholder="*****"/>
+                </div>
+                <div style="text-align: center; margin-top: 20px;">
+                    <input class="btn btn-secondary" type="submit" value="ВОЙТИ"/>
+                </div>
+            </form>
+        </div>
+        <div class="col-sm-3"></div>
+    </div>
+</div>
+
+</body>
+</html>
